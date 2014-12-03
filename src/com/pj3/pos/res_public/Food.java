b7e296f65935a3e3998a78f4df1ecd8f98a49dbb
truongdxq <dangxuantruongbkhn@gmail.com>
2014-12-03 21:58:00 +0700

    Them Serializable de Co the truyen doi tuong Food giua cac Activity

--- src/com/pj3/pos/res_public/Food.java
+++ src/com/pj3/pos/res_public/Food.java
@@ -3,21 +3,24 @@
  */
 package com.pj3.pos.res_public;
 
+import java.io.Serializable;
+
 /**
  * @author LÍCÙng
- *
+ * 
  */
-public class Food {
+public class Food implements Serializable{
 	private int m_food_id;
 	private String m_name;
 	private int m_price;
 	private String m_image;
 	private Boolean m_status;
-	
-	public Food(){
-		
+	private String m_option;
+
+	public Food() {
+
 	}
-	
+
 	public Food(int m_food_id, String m_name, int m_price, String m_image,
 			Boolean m_status) {
 		super();
@@ -27,8 +30,7 @@ public class Food {
 		this.m_image = m_image;
 		this.m_status = m_status;
 	}
-	
-	
+
 	public Food(String m_name, int m_price, String m_image, Boolean m_status) {
 		super();
 		this.m_name = m_name;
@@ -37,35 +39,51 @@ public class Food {
 		this.m_status = m_status;
 	}
 
-
 	public int getM_food_id() {
 		return m_food_id;
 	}
+
 	public void setM_food_id(int m_food_id) {
 		this.m_food_id = m_food_id;
 	}
+
 	public String getM_name() {
 		return m_name;
 	}
+
 	public void setM_name(String m_name) {
 		this.m_name = m_name;
 	}
+
 	public int getM_price() {
 		return m_price;
 	}
+
 	public void setM_price(int m_price) {
 		this.m_price = m_price;
 	}
+
 	public String getM_image() {
 		return m_image;
 	}
+
 	public void setM_image(String m_image) {
 		this.m_image = m_image;
 	}
+
 	public Boolean getM_status() {
 		return m_status;
 	}
+
 	public void setM_status(Boolean m_status) {
 		this.m_status = m_status;
+	}
+
+	public String getM_option() {
+		return m_option;
+	}
+
+	public void setM_option(String m_option) {
+		this.m_option = m_option;
 	}
 }
